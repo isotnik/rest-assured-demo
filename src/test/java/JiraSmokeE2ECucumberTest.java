e08d0c5ce5dd9441e35798806f3e3b6356c03753
Illia Sotnyk <sotnikib@gmail.com>
2024-06-03 14:30:24 -0600

    Add prettier html reporting for cucumber tests

--- src/test/java/JiraSmokeE2ECucumberTest.java
+++ src/test/java/JiraSmokeE2ECucumberTest.java
@@ -2,10 +2,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/java/features",
+        features = "src/test/java/features/JiraSmoke.feature",
         glue = "stepDefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports/cucumber-html-report.html"},
-        monochrome = true
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-html-report.html",
+                "json:target/json-reports/cucumber-report.json"
+        }
 )
 public class JiraSmokeE2ECucumberTest extends AbstractTestNGCucumberTests {
 
